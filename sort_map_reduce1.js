7d17b968fb8a0bca8ea36aa5a0b3587f2ce31283
Nguyen Le <bsbnguyen@gmail.com>
2020-04-26 17:55:14 -0700

    add new file

---
+++ sort_map_reduce1.js
@@ -0,0 +1,32 @@
+/**
+ * Dữ liệu sinh viên Coders.Tokyo School là một array các object như sau
+ * Viết function trả về 1 array gồm 3 người có điểm cao nhất sắp xếp theo thứ tự điểm giảm dần. 
+ * Mục đích để hiển thị lên bảng Leaderboard
+ * Gợi ý: sử dụng sort và slice
+ */
+
+var students = [
+    { name: 'A', score: 100 },
+    { name: 'B', score: 10 },
+    { name: 'C', score: 101 },
+    { name: 'D', score: 50 },
+    { name: 'E', score: 75 }
+];
+
+function getTopStudents(students) {
+    // Write code here...
+    return students.sort(function(a,b){
+        return b.score-a.score;
+    }).reduce(function(a,v){
+        a.push(v.name);
+        return a;
+    },[]).slice(0,3);
+    
+//       return students.sort(function(a,b){
+//         return b.score - a.score;
+//     }).map(function(v){
+//         return v.name;
+//     }).slice(0,3);
+}
+
+console.log(getTopStudents(students));
